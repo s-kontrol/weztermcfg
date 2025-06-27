9e1473ad40255598d0dfee509568490b6783358e
mruiz <110749352+s-kontrol@users.noreply.github.com>
2025-06-27 01:26:25 -0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,12 +5,36 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
-config.font = wezterm.font("Cascadia Code")
+config.font = wezterm.font("Cascadia Code", { weight = "Regular" })
 config.color_scheme = "Campbell"
+config.font_size = 11.5
+config.freetype_load_target = "Light" -- improves clarity
+config.freetype_render_target = "Normal"
 config.colors = {
-	background = "#1e1e1e", -- gray
+	background = "#1f1f1f",
+	foreground = "#e5e5e5",
+	ansi = {
+		"#0c0c0c",
+		"#d80000",
+		"#13a10e",
+		"#c19c00",
+		"#0037da",
+		"#881798",
+		"#3a96dd",
+		"#cccccc",
+	},
+	brights = {
+		"#767676",
+		"#ff4b4b",
+		"#16c60c",
+		"#f9f1a5",
+		"#3b78ff",
+		"#b4009e",
+		"#61d6d6",
+		"#ffffff",
+	},
 }
-config.window_background_opacity = 0.99 -- 1.0 = opaque, 0.0 = fully transparent
+config.window_background_opacity = 1.0 -- 1.0 = opaque, 0.0 = fully transparent
 
 config.enable_tab_bar = true
 if wezterm.target_triple:find("windows") then
